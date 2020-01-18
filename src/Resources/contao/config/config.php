29bdeeb8c3954890971fc7c64a20a86e607d6dba
Bastian Ullrich <bastian.ullrich@iem.thm.de>
2020-01-18 15:44:18 +0100

    Entfernen überflüssiger Dateien

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,10 +3,6 @@
 $GLOBALS['FE_MOD']['miscellaneous']['helloWorld'] = 'Baul\TestBundle\Module\HelloWorldModule';
 
 //Backend modules
-$GLOBALS['BE_MOD']['examia']['meinModul'] = [
-    'tables' => ['tl_meinModul'],
-    'callback' => 'Baul\TestBundle\MyClass'
-];
 
 $GLOBALS['BE_MOD']['examia']['exams'] = [
     'tables' => ['tl_exams'],
